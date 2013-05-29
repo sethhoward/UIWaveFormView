b2b9e99e941ad195acbeb2cd6b4de62ae83497ed
Seth Howard <spatialreasoning@gmail.com>
2013-05-29 10:16:34 -0600

    Initial Commit

---
+++ WavePreview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WavePreview
+//
+//  Created by Seth Howard on 5/29/13.
+//  Copyright (c) 2013 Seth Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
